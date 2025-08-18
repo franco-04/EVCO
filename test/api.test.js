153c120bab6170a063cc7a6861c01e61be0456c8
Franco-04 <davidfranco7329@gmail.com>
2025-08-17 20:06:51 -0600

    add: se agregaron pruebas para /hits/value

--- test/api.test.js
+++ test/api.test.js
@@ -1,10 +1,16 @@
 const request = require('supertest');
 const app = require('../src/index');
 
-describe('GET /', () => {
-  it('responde 200 y un JSON con message', async () => {
+describe('API Tests', () => {
+  it('GET / responde 200 y un JSON con message', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
     expect(res.body).toHaveProperty('message');
+  });
+
+  it('GET /hits/value responde con count', async () => {
+    const res = await request(app).get('/hits/value');
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toHaveProperty('count');
   });
 });
