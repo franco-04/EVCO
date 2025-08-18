d555c2736196c78f250229e9354b22863e032e24
Franco-04 <davidfranco7329@gmail.com>
2025-08-17 20:09:41 -0600

    Cambios test

--- test/api.test.js
+++ test/api.test.js
@@ -1,16 +1,10 @@
 const request = require('supertest');
 const app = require('../src/index');
 
-describe('API Tests', () => {
-  it('GET / responde 200 y un JSON con message', async () => {
+describe('GET /', () => {
+  it('responde 200 y un JSON con message', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
     expect(res.body).toHaveProperty('message');
-  });
-
-  it('GET /hits/value responde con count', async () => {
-    const res = await request(app).get('/hits/value');
-    expect(res.statusCode).toBe(200);
-    expect(res.body).toHaveProperty('count');
   });
 });
